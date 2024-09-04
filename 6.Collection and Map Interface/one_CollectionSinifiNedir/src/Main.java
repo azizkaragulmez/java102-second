8a92c061956a72ed4b37738b8f9c4eefdc9128ae
Aziz Karagülmez <123738266+azizkaragulmez@users.noreply.github.com>
2024-09-04 17:20:27 +0300

    what is collection

---
+++ 6.Collection and Map Interface/one_CollectionSinifiNedir/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //Çok sık kullanıcağımız bir yapıdır.
+        //içerisinde tablo bulunmaktadır.
+        System.out.println("Hello world!");
+    }
+}
